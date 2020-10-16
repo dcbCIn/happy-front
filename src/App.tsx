6f1a21c85ad9ddf6383b5482f838a68602622f6f
Daniel Brandão <dcb@cin.ufpe.br>
2020-10-15 21:51:45 -0300

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>A clean Happy app</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>Visit orphanages and change children's lives</p>
+        </main>
+
+        <div className="location">
+          <strong>Recife</strong>
+          <span>Pernambuco</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
